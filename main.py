3130a3a819b06b16c5f306417f44626139a20fc7
KeppFit <ahidden7.0@gmail.com>
2023-09-17 20:19:07 +0200

    final step of boot dev assignement

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ def main():
     file_content = open_file(file)
     words_number = count_words(file_content)
     letts = letters(file_content)
+    list = [(k, v) for k, v in letts.items()]
+    list.sort(key= lambda x: x[1], reverse= True)
+    report(words_number, list)
 #
 #
 def open_file(file):
     with open(file) as f:
         content = f.read()
-    file_content = content.split()
-    return file_content
+    return content.split()
 #
 #
 def count_words(file_content):
@@ -31,12 +33,20 @@ def letters(file_content):
                 file_content[i][y].lower() not in letters
             ):
                 letters[file_content[i][y].lower()] = 1
-    print(letters)
     return letters
 #
 #
 def is_alphabetic(character):
     return (character.lower() >= 'a' and character.lower() <= 'z')
+#
+#
+def report(words_number, list):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words_number} was found in the document")
+    print("")
+    for element in list:
+        print(f" The '{element[0]}' character was found {element[1]} times")
+    print("--- End report ---")
 #
 #
 main()
